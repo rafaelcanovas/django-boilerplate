6b0e85c44a59a3c0a34a1b5da609bcd0767bae9e
Rafael Canovas <rafaelcanovas@me.com>
2014-10-29 15:13:32 -0200

    Rename SECRET_KEY env var to DJANGO_SECRET_KEY

--- project_name/settings/production.py
+++ project_name/settings/production.py
@@ -4,4 +4,4 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 
 # In production the SECRET_KEY must be retrieved from the environment
-SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', SECRET_KEY)
